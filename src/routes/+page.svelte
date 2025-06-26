090aab04abe93774a82d64ca58e5334626f1456c
Fujisawa Yuki <g031y148yuki@gmail.com>
2025-06-26 18:50:55 +0900

    Remove: トップページを削除

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { goto } from '$app/navigation';
-  import { onMount } from 'svelte';
-
-  // ルートページ（/）にアクセスした際に、/userにリダイレクトする
-  onMount(() => {
-    goto('/user', { replaceState: true });
-  });
-</script>
