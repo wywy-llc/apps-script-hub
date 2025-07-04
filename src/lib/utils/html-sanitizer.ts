efc29bc90985a51259ec78b2c8ecd0f2288f64f5
Fujisawa Yuki <g031y148yuki@gmail.com>
2025-07-05 01:41:47 +0900

    Mod: ALLOWED_URI_REGEXPの正規表現を修正

--- src/lib/utils/html-sanitizer.ts
+++ src/lib/utils/html-sanitizer.ts
@@ -46,7 +46,7 @@ const SANITIZE_CONFIG = {
   ALLOWED_ATTR: ['href', 'title', 'target', 'rel', 'class', 'id', 'lang'],
   // リンクのプロトコル制限
   ALLOWED_URI_REGEXP:
-    /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|sms|cid|xmpp):|[^a-z]|[a-z+.-]+(?:[^a-z+.-:]|$))/i, // cspell:ignore callto xmpp
+    /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|sms|cid|xmpp):|[^a-z]|[a-z.+-]+(?:[^a-z.+-]|$))/i, // cspell:ignore callto xmpp
 };
 
 /**
