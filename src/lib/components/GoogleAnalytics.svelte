8e1d19c270434d0f869e9acce801bbe8acb328e2
Fujisawa Yuki <g031y148yuki@gmail.com>
2025-07-07 00:25:04 +0900

    Add: Google Analyticsコンポーネントをヘッダーに追加

---
+++ src/lib/components/GoogleAnalytics.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { browser, dev } from '$app/environment';
+
+  // 本番環境でのみGoogleアナリティクスを読み込む
+  const shouldLoadGA = browser && !dev;
+</script>
+
+<svelte:head>
+  {#if shouldLoadGA}
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-DQ4L0NYB3W"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+
+      gtag('config', 'G-DQ4L0NYB3W');
+    </script>
+  {/if}
+</svelte:head>
