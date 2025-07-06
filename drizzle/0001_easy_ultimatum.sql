21620674d163dfbc2a6252b6c062f88d5fd9d26f
Fujisawa Yuki <g031y148yuki@gmail.com>
2025-07-06 21:05:07 +0900

    Mod: 新しいライブラリおよびライブラリサマリーテーブルを追加し、関連する制約を設定

--- drizzle/0001_easy_ultimatum.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "library_summary" ADD COLUMN "seo_title_ja" text;--> statement-breakpoint
-ALTER TABLE "library_summary" ADD COLUMN "seo_title_en" text;--> statement-breakpoint
-ALTER TABLE "library_summary" ADD COLUMN "seo_description_ja" text;--> statement-breakpoint
-ALTER TABLE "library_summary" ADD COLUMN "seo_description_en" text;--> statement-breakpoint
-ALTER TABLE "library" DROP COLUMN "readme_content";
